462aa7c5c7477f5c25b0cb79f0b241766df83a40
magmesser <magmesser@gmail.com>
2023-01-16 18:46:12 -0500

    Changed format of dogsData.js to dogs.json. Added code to seeds/index.js

---
+++ seeds/index.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { Cat, Dog, User } = require('../models');
+
+const catData = require('./cats.json');
+const dogData = require('./dogs.json');
+const userData = require('./users.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Cat.bulkCreate(catData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Dog.bulkCreate(dogData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
