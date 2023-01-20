98fc33e79f7e1408fc01ab547c866f67282212c1
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-01-19 19:26:06 -0500

    added dog routes.js

---
+++ controllers/api/dogRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Dog } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try{
+        const newDog = await Dog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newDog);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const dogData = await Dog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if(!dogData) {
+            res.status(404).json({message: 'No dog with that id.' });
+            return;
+        }
+
+        res.status(200).json(dogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
