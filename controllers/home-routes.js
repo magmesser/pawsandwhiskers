47592dfad5c67b8ba5c2d3a0937c6e1ca4bd9d2f
magmesser <magmesser@gmail.com>
2023-01-21 14:57:05 -0500

    Added single-pet.handlebars and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -19,17 +19,37 @@ router.get("/login", (req, res) => {
   res.render("homepage");
 });
 
+// GET all pets
 router.get("/pets", async (req, res) => {
   try {
     const dogData = await Dog.findAll({});
-
+    const catData = await Cat.findAll({});
     const dogs = dogData.map((dog) => dog.get({ plain: true }));
+    const cats = catData.map((cat) => cat.get({ plain: true }));
 
     res.render("pets", {
-      dogs,
+      dogs, cats,
       logged_in: true
     });
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET dog by id/image link
+router.get("/pets/dog/:id", async (req, res) => {
+  try {
+    const dogData = await Dog.findOne({
+      where: {id: req.params.id}
+    });
+
+    const dog = dogData.get({ plain: true });
+
+    res.render('single-pet', { 
+      ...dog,
+      logged_in: req.session.logged_ing });
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
